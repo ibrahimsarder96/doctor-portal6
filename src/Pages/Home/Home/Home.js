93285708eb6ceea5812b59af4db05d5270a50b07
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-02-12 03:01:07 +0600

    Services & MakeAppointment add

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Info from '../Info/Info';
+import MakeAppointment from '../MakeAppointment/MakeAppointment';
+import Services from '../Services/Services';
 
 const Home = () => {
   return (
     <div className='mx-5'>
       <Banner></Banner>
       <Info></Info>
+      <Services></Services>
+      <MakeAppointment></MakeAppointment>
     </div>
   );
 };
