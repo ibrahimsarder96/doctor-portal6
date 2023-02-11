46ae8fac6039e5f0cf5a54952c049ff8e61a5779
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-02-12 03:17:30 +0600

    footer add

---
+++ src/Pages/Home/Contact/Contact.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PrimaryButton from '../../Shared/PrimaryButton/PrimaryButton';
+import appointment from '../../../assets/images/appointment.png';
+const Contact = () => {
+  return (
+    <section style={{
+      background: `url(${appointment})`
+    }} className='justify-center my-28 py-24'>
+      <div className='text-center'>
+        <h1 className='text-primary font-bold text-xl'>Contact Us</h1>
+        <h2 className='text-white text-3xl'>Stay connected with us</h2>
+      </div>
+      <div className='grid grid-cols-1 gap-3 justify-items-center'>
+      <input type="text" placeholder="Email Address" className="input input-bordered input-info w-full max-w-xs my-4 text-accent text-xl"/>
+      <input type="text" placeholder="Subject" className="input input-bordered input-info w-full max-w-xs text-accent text-xl" />
+      <textarea type="text" placeholder="Your message" className="input input-bordered input-info w-full max-w-xs h-36 text-accent text-xl" />
+      <PrimaryButton>Submit</PrimaryButton>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,37 @@
 import React from 'react';
-
+import footer from '../../../assets/images/footer.png';
 const Footer = () => {
+  const startYear = 2021
+  const currentYear = new Date().getFullYear();
   return (
-    <div className='mx-5'>
-      <h1>This is footer</h1>
-    </div>
+    <footer style={{
+      background: `url(${footer})`,
+      backgroundSize: 'cover'
+    }} className="p-10 ">
+ <div className="footer text-accent w-full 2xl:max-w-screen-xl">
+ <div>
+    <span className="footer-title">Services</span> 
+    <a className="link link-hover">Emergency Checkup</a>
+    <a className="link link-hover">Monthly Checkup</a>
+    <a className="link link-hover">Weekly Checkup</a>
+    <a className="link link-hover">Deep Checkup</a>
+  </div> 
+  <div>
+    <span className="footer-title">ORAL HEALTH</span> 
+    <a className="link link-hover">Fluoride Treatment</a>
+    <a className="link link-hover">Cavity Filling</a>
+    <a className="link link-hover">Teath Whitening</a>
+  </div> 
+  <div>
+    <span className="footer-title">OUR ADDRESS</span> 
+    <a className="link link-hover">New York - 101010 Hudson</a>
+    
+  </div>
+ </div>
+ <div className='my-10 text-center'>
+    <p className='text-accent'>Copyright © {currentYear} - All right reserved</p>
+  </div>
+</footer>
   );
 };
 
