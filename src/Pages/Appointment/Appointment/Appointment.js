b41eca740ed76bd4e9a53a10b31e71d2270f73fa
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-02-13 23:57:22 +0600

    appointmentBanner componnet add

--- src/Pages/Appointment/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment/Appointment.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import AppointmentBanner from '../AppointmentBanner/AppointmentBanner';
 
 const Appointment = () => {
   return (
     <div>
-      <h1>This is appointment</h1>
+      <AppointmentBanner></AppointmentBanner>
     </div>
   );
 };
