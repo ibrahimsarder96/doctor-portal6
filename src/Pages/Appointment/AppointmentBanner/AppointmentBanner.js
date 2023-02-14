a02749a225145465abfc4a763d6164561e73da47
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-02-14 22:30:51 +0600

    daypicker add

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import chair from '../../../assets/images/chair.png';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
 const AppointmentBanner = () => {
+  // const [selectedData, setSelectedData] = (new Date());
   return (
     <header>
       <div className="hero">
   <div className="hero-content flex-col lg:flex-row-reverse">
     <img src={chair} alt='' className="max-w-sm rounded-lg shadow-2xl" />
     <div>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+    <DayPicker
+      // mode="single"
+      // selected={selectedData}
+      // onSelect={setSelectedData}
+      
+    />
     </div>
   </div>
 </div>
