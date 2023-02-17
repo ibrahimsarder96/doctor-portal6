8dead50e5b90957e8b409b2e3fc15a8a9220c123
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-02-17 21:54:58 +0600

    availableappointments

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import chair from '../../../assets/images/chair.png';
 import { DayPicker } from 'react-day-picker';
-import { format } from 'date-fns';
+import bg from '../../../assets/images/bg.png';
 const AppointmentBanner = () => {
-  const [selectedData, setSelectedData] = (new Date());
+  const [selectedData, setSelectedData] = useState(new Date());
+
   return (
     <header>
-    <div className="hero">
+    <div
+      style={{
+        background: `url(${bg})`,
+        backgroundSize: 'cover'
+      }}  
+     className="hero">
     <div className="hero-content flex-col lg:flex-row-reverse">
     <img src={chair} alt='' className="max-w-sm rounded-lg shadow-2xl" />
     <div>
@@ -14,7 +20,6 @@ const AppointmentBanner = () => {
       mode="single"
       selected={selectedData}
       onSelect={setSelectedData}
-      
     />
     </div>
   </div>
