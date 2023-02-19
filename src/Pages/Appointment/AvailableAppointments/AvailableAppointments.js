2d94be5a781e54b65fc1cb16e08d1588c4fcc92e
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-02-19 22:02:59 +0600

    services add

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -4,9 +4,24 @@ import React from 'react';
 
 const AvailableAppointments = ({selectedDate}) => {
   return (
-    <div>
-      <p className='text-center text-primary font-bold'>You have selected date: {format(selectedDate, 'PP')}</p>
+    <div className='my-28'>
+    <h1 className='text-secondary  text-bold text-center text-xl my-24'>Available Services on  {format(date, 'PP')}</h1>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+      {
+        services?.map(service => <Service
+        key={service._id}
+        service={service}
+        setTreatment={setTreatment}
+        ></Service>)
+      }
     </div>
+  { treatment && <BookingModal 
+  treatment={treatment}
+  date={date}
+  refetch={refetch}
+  setTreatment={setTreatment}
+  ></BookingModal>}
+  </div>
   );
 };
 
